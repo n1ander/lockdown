8b3b55f2bbcfbfddf97dd0f27ddde584b3393bdf
notdeckvrd <73615640+notdeckvrd@users.noreply.github.com>
2021-07-19 16:18:02 -0400

    feat: create file handle
    Pass file as argument to main. Print hello world to console if
    successful.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+#include "stdio.h"
 #include "encrypt.h"
 
-int main(){
-  std::cout << "Hello world!\n" << std::endl;
+int main(int argc, char* argv[]){
+  FILE *fp = fopen(argv[1], "r");
+  if(fp == NULL){
+    std::cout << "Could not read the file!\n" << std::endl;
+  }
+  std::cout << "Hello worldddd!\n" << std::endl;
   encrypt e;
   return 0;
 }
